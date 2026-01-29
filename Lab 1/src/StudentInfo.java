023d333199adab6c741861ebef3c02f2de8f4851
focusass <d.kasymtaev@mail.ru>
2026-01-29 14:41:32 +0500

    Add files via upload

---
+++ Lab 1/src/StudentInfo.java
@@ -0,0 +1,16 @@
+class StudentInfo {
+
+    public static void main(String[] args) {
+
+        String fullName = "Касымтаев Даулет";
+        String group = "ИС 24-21";
+        String specialty = "Информационные системы";
+        int course = 2;
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("Курс: " + course);
+        System.out.println("Специальность: " + specialty);
+    }
+}
